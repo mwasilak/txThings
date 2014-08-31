c33450d45c55c8b86eb437221d8bed9d506e2050
Maciej Wasilak <wasilak@gmail.com>
2014-08-31 11:58:38 +0200

    Fix bugs in clientPUT.py example

--- examples/clientPUT.py
+++ examples/clientPUT.py
@@ -3,9 +3,7 @@ Created on 08-09-2012
 
 @author: Maciej Wasilak
 '''
-import struct
-import random
-import copy
+
 import sys
 
 from twisted.internet.defer import Deferred
@@ -19,9 +17,9 @@ import txthings.resource as resource
 
 class Agent():
     """
-    Example class which performs single PUT request to localhost
-    port 5683 (official IANA assigned CoAP port), URI "/other/block".
-    Request is sent 2 seconds after initialization.
+    Example class which performs single PUT request to iot.eclipse.org
+    port 5683 (official IANA assigned CoAP port), URI "/large-update".
+    Request is sent 1 second after initialization.
 
     Payload is bigger than 64 bytes, and with default settings it
     should be sent as several blocks.
@@ -29,18 +27,20 @@ class Agent():
 
     def __init__(self, protocol):
         self.protocol = protocol
-        reactor.callLater(2, self.putResource)
+        reactor.callLater(1, self.putResource)
 
     def putResource(self):
-        payload = "Poland CAN into space!!! Poland MUST into space!!! Poland WILL into space!!!!"
+        payload = "Riders on the storm.\nRiders on the storm.\nInto this house we're born\nInto this world we're thrown"
         request = coap.Message(code=coap.PUT, payload=payload)
-        request.opt.uri_path = ("other", "block")
-        request.remote = ('127.0.0.1', coap.COAP_PORT)
+        request.opt.uri_path = ("large-update",)
+        request.opt.content_format = coap.media_types_rev['text/plain']
+        request.remote = ('198.41.30.241', coap.COAP_PORT)
         d = protocol.request(request)
         d.addCallback(self.printResponse)
 
     def printResponse(self, response):
-        print 'Result: ' + response.payload
+        print 'Response Code: ' + coap.responses[response.code]
+        print 'Payload: ' + response.payload
 
 log.startLogging(sys.stdout)
 
