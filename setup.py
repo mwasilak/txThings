d2d0e1abb460a3240c35f9c6242c975c2e11b07b
Maciej Wasilak <wasilak@gmail.com>
2014-08-31 14:27:42 +0200

    Add setup.py for txThings installation with setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='txThings',
+      version='0.1',
+      description='CoAP protocol implementation for Twisted Framework',
+      author='Maciej Wasilak',
+      author_email='wasilak@gmail.com',
+      url='https://github.com/siskin/txThings/',
+      packages=find_packages(exclude=["*.test", "*.test.*"]),
+      install_requires = ['twisted>=14.0.0'],
+     )
